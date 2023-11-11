f98a9d2ead98c5c4b6ff7ad612e247fd57a1061d
mostafa.m.tawfik <mostafa.mohamed97@eng-st.cu.edu.eg>
2023-11-11 16:22:22 +0200

    test

--- ECU_Node1/Debug/Core/Src/UART_program.d
+++
@@ -1,8 +0,0 @@
-Core/Src/UART_program.o: ../Core/Src/UART_program.c \
- ../Core/Inc/STD_TYPES.h ../Core/Inc/BIT_MATH.h ../Core/Inc/UART_config.h \
- ../Core/Inc/UART_interface.h ../Core/Inc/UART_private.h
-../Core/Inc/STD_TYPES.h:
-../Core/Inc/BIT_MATH.h:
-../Core/Inc/UART_config.h:
-../Core/Inc/UART_interface.h:
-../Core/Inc/UART_private.h:
