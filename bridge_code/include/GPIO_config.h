fa562a5b62e48be6e79c5df12515228d30896b7d
mostafa.m.tawfik <mostafa.mohamed97@eng-st.cu.edu.eg>
2023-11-12 22:19:28 +0200

    Node 1 "bridge node"  code that will receive application code and send it to other Node 2  "Bootloader "by CAN""

---
+++ bridge_code/include/GPIO_config.h
@@ -0,0 +1,15 @@
+/*************************************************************/
+/* Author    : Abanoub nader & Mohamed mahmoud               */
+/* SWC       : GPIO                       					 */
+/* Layer     : MCAL                       					 */
+/* Version   : 1.0                        					 */
+/* Date      : October 15, 2023          					 */
+/* Last Edit : N/A                        					 */
+/*************************************************************/
+
+#ifndef _GPIO_CONFIG_H_
+#define _GPIO_CONFIG_H_
+
+
+
+#endif
